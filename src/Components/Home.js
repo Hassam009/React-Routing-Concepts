6eabee102f2ff44b87e2050b684e3886cf265311
Hassam Amer <hasideveloper@gmail.com>
2024-01-06 00:33:22 +0500

    I added button in Home Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div>
       <h1>I am Home</h1>
+    <button type="button" class="btn btn-primary">Read More</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
     </div>
   )
 }
