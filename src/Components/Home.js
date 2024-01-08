d74ec6f856812f4b5a4524df0c07d0c6e1f2ff1a
Hassam Amer <hasideveloper@gmail.com>
2024-01-08 13:37:21 +0500

    Now my Props Concepts is clear.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import Card from './Card'
 
 const Home = (props) => {
   // const data=props.title;
   return (
-    <div className='Container1'>
+    <>
       <h1>{props.title}</h1>
+      <div style={{ display: 'flex', gap: '20px' }}>
+      <Card TitleName="Image 1" src="/Images/B4.jpg" ></Card>
+      <Card TitleName="Image 2" src="/Images/B5.jpg" ></Card>
+      <Card TitleName="Image 3" src="/Images/B2.jpg" ></Card>
+   
+    </div>
     <button type="button" class="btn btn-primary">Read More</button>
     <button type="button" class="btn btn-secondary">Secondary</button>
     <button type="button" class="btn btn-success">Success</button>
-    </div>
+    </>
+    
   )
 }
 
