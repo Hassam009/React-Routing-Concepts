4fd7ebf99e2d167ba626224e9bbf9e3c9dfb22b5
Hassam Amer <hasideveloper@gmail.com>
2024-01-13 18:53:51 +0500

    Today i will apply useContext Hooks

---
+++ src/Props/Components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const Services = () => {
+  return (
+    <>
+      <Header></Header>
+   <h1>Now i will apply useContext Hook</h1>
+    </>
+ 
+  )
+}
+
+export default Services
