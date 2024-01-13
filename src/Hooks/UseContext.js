ff650009af4046199f61753240740ca3d5bbbd85
Hassam Amer <hasideveloper@gmail.com>
2024-01-13 19:32:59 +0500

    Now i applied same thing to the child

--- src/Hooks/UseContext.js
+++ src/Hooks/UseContext.js
@@ -5,6 +5,7 @@ const AppContext= createContext();
 const AppProvider=({children})=>{
 const userData={
 name:"i am Data of Super Child dont pass it to child or parent",
+childData:"I am Data of Child"
 };
 
 return <AppContext.Provider value={userData}>{children}</AppContext.Provider>
--- src/Props/Components/Child.js
+++ src/Props/Components/Child.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import SuperChild from './SuperChild'
+import { AppContext } from '../../Hooks/UseContext'
 const Child = () => {
+
+    const userData= useContext(AppContext)
+
   return (
     <div>
          <div style={{ textAlign: 'center', marginTop: '50px' }}>
          <h1>I am Child Component</h1>
+         <h2>Mera Data yeh Hai "{userData.childData}"</h2>
          </div>
     
       <SuperChild></SuperChild>
