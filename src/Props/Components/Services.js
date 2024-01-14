8e255d4ef4ba492821f8b82def78ec7ae9ae6f0b
Hassam Amer <hasideveloper@gmail.com>
2024-01-14 23:42:53 +0500

    I used list and keys concept in services Page

--- src/Props/Components/Services.js
+++ src/Props/Components/Services.js
@@ -3,12 +3,19 @@ import React from 'react'
 import Parent from './Parent'
 
 const Services = () => {
+  const listItems=["Item 1", "Item 2", "Item 3"]
   return (
     <>
     
      <Parent></Parent>
    
+<h2>Now i used the Concepts of List and Keys</h2>
+<ul>
+  {listItems.map((Items, index)=>(
+<li>{Items}</li>
 
+  ))}
+</ul>
     </>
  
   )
